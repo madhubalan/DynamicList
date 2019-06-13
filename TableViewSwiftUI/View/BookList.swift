8048e35fc498ee253e77b231a02f2cd2d33f6fbe
Madhubalan <kmadhub20@gmail.com>
2019-06-13 10:10:59 +0530

    Initial commit

---
+++ TableViewSwiftUI/View/BookList.swift
@@ -0,0 +1,23 @@
+//
+//  BookList.swift
+//  TableViewSwiftUI
+//
+//  Created by Madhu on 13/06/19.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import SwiftUI
+
+struct BookList : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct BookList_Previews : PreviewProvider {
+    static var previews: some View {
+        BookList()
+    }
+}
+#endif
